52579b9f4183cf402cf056fb2b558d6c6b4e7503
Jeff Chiu <jwchiu82@gmail.com>
2021-07-26 20:49:36 -0700

    add approvalcard.js component and pass commentdetail as a prop

--- src/index.js
+++ src/index.js
@@ -2,28 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        avatar={faker.image.avatar()}
-        content={faker.lorem.sentence()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 2:00PM"
-        avatar={faker.image.avatar()}
-        content={faker.lorem.sentence()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday at 5:00PM"
-        avatar={faker.image.avatar()}
-        content={faker.lorem.sentence()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          avatar={faker.image.avatar()}
+          content={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00PM"
+          avatar={faker.image.avatar()}
+          content={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00PM"
+          avatar={faker.image.avatar()}
+          content={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
     </div>
   );
 };

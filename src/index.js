3dca9437f5beb268cb6f7248d725ecccd0245b40
Jeff Chiu <jwchiu82@gmail.com>
2021-07-26 20:36:42 -0700

    pass avatar and content props to commentDetail.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        avatar={faker.image.avatar()}
+        content={faker.lorem.sentence()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00PM"
+        avatar={faker.image.avatar()}
+        content={faker.lorem.sentence()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:00PM"
+        avatar={faker.image.avatar()}
+        content={faker.lorem.sentence()}
+      />
     </div>
   );
 };
